563356804cdbc1778aa2a853e3ab628fc93c9571
Caio Zidane <caiozidane@gmail.com>
2020-01-22 08:55:40 -0400

    bug fixes

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -3,23 +3,23 @@ const Dev = require('../models/Dev');
 const parseStringAsArray = require('../utils/parseStringAsArray');
 const { findConnections, sendMessage } = require('../websocket');
 
+
 module.exports = {
-    async index(request, response){
+    async index(request, response) {
         const devs = await Dev.find();
 
         return response.json(devs);
     },
 
-    async store(request, response){
+    async store(request, response) {
         const { github_username, techs, latitude, longitude } = request.body;
 
         let dev = await Dev.findOne({ github_username });
 
-        if(!dev){
-
+        if (!dev) {
         const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
     
-        const { name, avatar_url, bio} = apiResponse.data;
+        const { name = login, avatar_url, bio } = apiResponse.data;
         
         const techsArray = parseStringAsArray(techs);
     
@@ -28,11 +28,7 @@ module.exports = {
             coordinates: [longitude, latitude],
         }
     
-<<<<<<< HEAD
-         const dev = await Dev.create({
-=======
         dev = await Dev.create({
->>>>>>> d4ea5e4302ca8302e51e48cb3d3f8331376a24cd
             github_username,
             name,
             avatar_url,
@@ -49,23 +45,9 @@ module.exports = {
             techsArray,
         )
 
-<<<<<<< HEAD
-        //filtra as conexões que estão à no máximo 10km de distancia
-        // e que o novo dev tenha pelo menos uma das tecnologias filtradas
-
-        const sendSocketMessageTo = findConnections(
-            {latitude, longitude},
-            techsArray,
-        )
-
-        sendMessage(sendSocketMessageTo, 'newDev', dev);
-
-        }
-=======
         sendMessage(sendSocketMessageTo, 'newDev', dev);
     }
->>>>>>> d4ea5e4302ca8302e51e48cb3d3f8331376a24cd
-
+    
         return response.json(dev);
-    },
+},
 };
--- backend/src/utils/parseStringAsArray.js
+++ backend/src/utils/parseStringAsArray.js
@@ -1,7 +1,3 @@
 module.exports = function parseStringAsArray(arrayAsString){
-<<<<<<< HEAD
     return arrayAsString.split(',').map(tech => tech.trim().toLowCase);
-=======
-    return arrayAsString.split(",").map(tech => tech.trim().toLowCase);
->>>>>>> d4ea5e4302ca8302e51e48cb3d3f8331376a24cd
 }
